6a9ad98c6efe8fbf4e14da7560df91bd0d562d17
Mohamed Hussein <m.hussien999m@gmail.com>
2023-09-26 13:55:15 +0300

    firstCommit

---
+++ ABC University/ABC University/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ABC_University.Startup))]
+namespace ABC_University
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
